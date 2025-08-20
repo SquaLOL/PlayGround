bec9fcb95c4eb1c9b1ca7e961b3acc00c5c91763
Nikolai Ankin <nikolaiankin11@gmail.com>
2025-08-19 22:31:03 -0700

    update

--- 14 august.py
+++ 14 august.py
@@ -164,18 +164,60 @@ def insert_barrels(warehouse, barrels):
 
     return warehouse
 
-
-
-
-
-
-
 print(insert_barrels(['0','','','0','','','','0'],['0','0']))
 
 
+array = [1,2,3]
+array.append('1')
+array.append(False)
+array.append({1})
+array.append([{1: 'Name'}])
+print(array)
+
+
+def binary_to_string(binary):
+    binary_chunks = binary.split('0b')[1::1]
+    result = ''.join(chr(int(b, 2)) for b in binary_chunks)
+    return result
+
+print(binary_to_string('0b10000110b11000010b1110100'))
 
 
 
+def print_odds(data):
+    pass
+
+
+
+if __name__ == '__main__':
+    binary_to_string('0b10001111110110b11000010b1110100')
+
+
+
+class Person:
+    name: str
+    age: int
+    extra_info: dict = {}
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        self.extra_info = {} #!!!!!!!!!!!!!!!!!!!!!!!!
+
+    def __getitem__(self, item,):
+        return self.extra_info[item]
+
+    def __str__(self):
+        return f'{self.name}, {self.age}, {self.extra_info}'
+
+vasya = Person('Vasya', 21)
+masha = Person('Masha', 22)
+
+vasya.extra_info['description'] = 'student'
+masha.extra_info['description'] = 'workworkwork'
+
+print(vasya)
+print(masha)
 
 
 
