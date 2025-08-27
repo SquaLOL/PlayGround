601c88c4ce5790b48e3b037cc2593448befe1c8c
Nikolai Ankin <nikolaiankin11@gmail.com>
2025-08-27 10:03:34 -0700

    update

--- 20.py
+++ 20.py
@@ -1,6 +1,5 @@
-
 def hello(word: str):
-    a,b = word.split()
+    a, b = word.split()
     ar, br = a[::-1], b[::-1]
     m = max(len(a), len(b))
     out = []
@@ -10,4 +9,22 @@ def hello(word: str):
         out.append(b[y] + ar[y])
     return '\n'.join(out)
 
-print(hello("Hello World"))
+
+# print(hello("Hello World"))
+
+
+def even_odd(arr):
+    if not arr:
+        return 0
+    result = arr[0]
+    multiply_next = True
+    for x in arr[1:]:
+        if multiply_next:
+            result *= x
+        else:
+            result += x
+        multiply_next = not multiply_next
+    return result
+
+
+print(even_odd([1, 2, 3]))
