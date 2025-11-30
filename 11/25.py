601c6a330a013be02be8433dac56d413d315177a
Nikolai Ankin <nikolaiankin11@gmail.com>
2025-11-29 22:43:14 -0700

    update

--- 11/25.py
+++ 11/25.py
@@ -28,7 +28,49 @@ def duty_free(price, discount, holiday_cost):
 
 
 def is_palindrome(s):
-    return s.lower() == s[::-1]
+    return s.lower() == s.lower()[::-1]
 
 
-print(is_palindrome('abbbac'))
+print(is_palindrome('abbac'))
+
+arr = [1, 2]
+
+
+def swap_values(args):
+    args[0], args[1] = args[1], args[0]
+
+
+def high_and_low(numbers):
+    numbers = numbers.split()
+    numbers = [i for i in numbers]
+    return str(max(numbers)) + ' ' + str(min(numbers))
+
+
+def sing():
+    song = ["99 bottles of beer on the wall, 99 bottles of beer.",
+            "Take one down and pass it around, 98 bottles of beer on the wall.",
+            "98 bottles of beer on the wall, 98 bottles of beer.",
+
+            "...and so on...",
+
+            "3 bottles of beer on the wall, 3 bottles of beer.",
+            "Take one down and pass it around, 2 bottles of beer on the wall.",
+            "2 bottles of beer on the wall, 2 bottles of beer.",
+            "Take one down and pass it around, 1 bottle of beer on the wall.",
+            "1 bottle of beer on the wall, 1 bottle of beer.",
+            "Take one down and pass it around, no more bottles of beer on the wall.",
+            "No more bottles of beer on the wall, no more bottles of beer.",
+            "Go to the store and buy some more, 99 bottles of beer on the wall."]
+
+    return song
+
+
+def bubblesort_once(arr):
+    new_array = arr[:]
+    for i in range(len(new_array) - 1):
+        if new_array[i] > new_array[i + 1]:
+            new_array[i], new_array[i + 1] = new_array[i + 1], new_array[i]
+    return new_array
+
+
+print(bubblesort_once([9, 7, 5, 3, 1, 2, 4, 6, 8]))
