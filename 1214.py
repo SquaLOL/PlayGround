2193ac14952d89a6f91bf18e7eacde8b50b311dd
Nikolai Ankin <nikolaiankin11@gmail.com>
2025-12-14 16:33:54 -0700

    update

--- 1214.py
+++ 1214.py
@@ -4,8 +4,16 @@ geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"]
 def goose_filter(birds):
     return [bird for bird in birds if bird not in geese]
 
+
 # l=[]
 #     for i in birds:
 #         if i not in geese:
 #             l.append(i)
 #     return(l)
+
+
+def sakura_fall(v):
+    return 400 / v if v > 0 else 0
+    if v <= 0:
+        return 0
+    return 400 / v
